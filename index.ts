a06f4375c92ddae42fe78c48ccd7b4c9e9157976
Christian Socias <Socias_93@hotmail.com>
2025-12-28 23:15:44 +0100

    added get api endpoint for items

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import categories from "./routes/categories";
+import items from "./routes/items";
 
 const app = express();
 const PORT = 5555;
 const CATEGORY_URL = "/api/categories";
+const ITEMS_URL = "/api/items";
 
 app.use(CATEGORY_URL, categories);
+app.use(ITEMS_URL, items);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
